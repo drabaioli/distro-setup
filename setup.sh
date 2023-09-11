c3d7908c550a0ab86e2e432c302adb1295703e69
Diego Andres Rabaioli <drabaioli@gmail.com>
2023-09-11 22:07:06 +0200

    Fix file path.

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ fi
 sudo ./scripts/install_apt_packages.sh
 
 # Install nerd fonts
-./scripts/install_nerd_font.sh
+./scripts/install_nerd_fonts.sh
 
 # Load terminal configuration
 ./scripts/load_terminal_config.sh
